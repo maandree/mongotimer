f94d86b506838259d1013391d2e034bc11f2dd7d
Mattias Andrée <maandree@operamail.com>
2015-11-27 22:29:24 +0100

    change license
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/mongoclock.c
+++ src/mongoclock.c
@@ -1,10 +1,20 @@
 /**
+ * mongoclock - Just a humongous clock for the terminal
+ * 
  * Copyright © 2013, 2015  Mattias Andrée  (maandree@member.fsf.org)
  * 
- * Copying and distribution of this file, with or without modification,
- * are permitted in any medium without royalty provided the copyright
- * notice and this notice are preserved.  This file is offered as-is,
- * without any warranty.  [GNU All Permissive License]
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define _GNU_SOURCE
 #include <stdio.h>
